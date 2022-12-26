c23a407f6730701faf22b34480d015891bb6b706
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-26 23:02:18 +0300

    Сделал первую задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+double[,] Create2DRandomArray (int rows, int colomns)
+{
+    double[,] newArray = new double[rows,colomns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < colomns; j++)
+        {
+            newArray[i,j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+    return newArray;
+}
+
+void Show2DArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            double result = Math.Round(array[i,j], 1);
+            Console.Write(result + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+double[,] myArray = Create2DRandomArray(m,n);
+Show2DArray(myArray);
