72090c78105a75e31a750782b33cfe8fde729eba
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-27 03:47:39 +0300

    Сделал третью задачу

--- Program.cs
+++ Program.cs
@@ -53,6 +53,66 @@
 // 17 -> такого числа в массиве нет
 
 
+// int[,] Create2DRandomArray (int rows, int colomns, int minValue, int maxValue)
+// {
+//     int[,] newArray = new int[rows,colomns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < colomns; j++)
+//         {
+//             newArray[i,j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray (int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// void ShowNumber (int[,] array, int poz1, int poz2)
+// {   
+//     if (poz1 > array.GetLength(0) || poz2 > array.GetLength(1)) Console.WriteLine($"По таким координатам не найдено число");
+//     else Console.WriteLine($"По таким координатам находится число {array[poz1,poz2]}");
+// }
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное возможное значение элемента массива:"); 
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива:");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите левую границу:");
+// int poz1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите правую границу:");
+// int poz2 = Convert.ToInt32(Console.ReadLine());
+
+
+// int[,] myArray = Create2DRandomArray(m,n,min,max);
+// Show2DArray(myArray);
+// ShowNumber(myArray, poz1, poz2);
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
 int[,] Create2DRandomArray (int rows, int colomns, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows,colomns];
@@ -79,10 +139,20 @@ void Show2DArray (int[,] array)
 }
 
 
-void ShowNumber (int[,] array, int poz1, int poz2)
-{   
-    if (poz1 > array.GetLength(0) || poz2 > array.GetLength(1)) Console.WriteLine($"По таким координатам не найдено число");
-    else Console.WriteLine($"По таким координатам находится число {array[poz1,poz2]}");
+void ShowNumber (int[,] array, int m, int n)
+{ 
+    
+    for (int i = 0; i < n; i++)
+    {
+        double result = 0;
+        for (int j = 0; j < m; j++)
+        {
+            result = result + array[j,i];
+        }
+        result = Math.Round(result / m, 1);
+        Console.WriteLine($"Среднее арифметическое столбца № {i+1}  равно {result}");
+    }
+    
 }
 
 Console.WriteLine("Введите количество строк");
@@ -93,12 +163,8 @@ Console.WriteLine("Введите минимальное возможное зн
 int min = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимально возможное значение элемента массива:");
 int max = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите левую границу:");
-int poz1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите правую границу:");
-int poz2 = Convert.ToInt32(Console.ReadLine());
 
 
 int[,] myArray = Create2DRandomArray(m,n,min,max);
 Show2DArray(myArray);
-ShowNumber(myArray, poz1, poz2);
+ShowNumber(myArray, m, n);
